4629d6bc2ef04c86b04dec7e2d09c5230da2139c
Paolo <pj.angeloni@gmail.com>
2024-09-29 17:36:04 -0400

    added comments

--- COMP5511/assignment1/BinaryFib.java
+++ COMP5511/assignment1/BinaryFib.java
@@ -3,7 +3,8 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.math.BigInteger; 
 
-// code fragment 5.10 from textbook
+// code fragment 5.10, from textbook, pg. 211
+// code fragment 5.13, from textbook, pg. 216
 public class BinaryFib {   
      // use BigInteger for accuracy of output on large fibonacci executions
     public static BigInteger binaryFibonacci(BigInteger n) {
@@ -17,34 +18,39 @@ public class BinaryFib {
     }
     
     public static void main(String[] args) {
+        // start execution time of main
         long startMainTime = System.nanoTime();
         // set PrintStream outside of try block to access is for execution time of overall `main` function
         PrintStream out = null;
         try {
             // use PrintStream to output executions (calculation results and timings) to a file
             out = new PrintStream("COMP5511/assignment1/BinaryFib_out.txt");
+            // every System output sent to file
             System.setOut(out);
 
             // set of testing values for binary fibonacci calculations
-            // removed 55, since it took almost 10 minutes!
-            int[] numbers = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+            // removed 60, 55 and 50, since it took over 10 minutes!
+            int[] numbers = {5, 10, 15, 20, 25, 30, 35, 40, 45};
 
             for (long number : numbers) {
                 // each call will have a separate execution time calculation
                 long startTime = System.nanoTime();
                 BigInteger binaryResult = binaryFibonacci(BigInteger.valueOf(number));
                 long endTime = System.nanoTime();
-                double duration = (endTime - startTime) / 1000000000.0;
-                out.println("binary Fibonnacci of: " + number + " = " + binaryResult);
-                out.println("execution time = " + duration + " seconds");
+
+                // execution time (nanoseconds) convert to seconds
+                double duration = (endTime - startTime) / 1_000_000_000.0;
+
+                out.println("\n binary Fibonnacci of: " + number + " = " + binaryResult);
+                out.println(String.format("\t execution time =  %.9f", duration ) + " seconds");
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        //output main function exec ution time to file
+        //output main function execution time at end of same file
         long endMainTime = System.nanoTime();
-        double mainDuration = (endMainTime - startMainTime) / 1000000000.0;
-        out.println("TOTAL execution time = " + mainDuration);
+        double mainDuration = (endMainTime - startMainTime) / 1_000_000_000.0;
+        out.println("\n\n TOTAL execution time = " + mainDuration);
     }
 }
--- COMP5511/assignment1/LinearFib.java
+++ COMP5511/assignment1/LinearFib.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.math.BigInteger;
 
-// code fragment 5.14 from textbook
+// code fragment 5.14 from textbook, pg. 217
 public class LinearFib {
 
     public static BigInteger[] linearFibonacci(int n) {
@@ -11,13 +11,16 @@ public class LinearFib {
             BigInteger[] result = {BigInteger.valueOf(n), BigInteger.ZERO};
             return result;
         } else {
+            // single recursive call
             BigInteger[] temp = linearFibonacci(n - 1);
-            BigInteger[] result = {temp[0].add(temp[1]), temp[0]};
+            // return an array, 0th index = sum of values
+            BigInteger[] result = {temp[0].add(temp[1]), temp[0], temp[1]};
             return result;
         }
     }
 
     public static void main(String[] args) {
+        // start execution time of main
         long startMainTime = System.nanoTime();
 
         // set PrintStream outside of try block to access is for execution time of overall `main` function
@@ -25,6 +28,7 @@ public class LinearFib {
         try {
             // use PrintStream to output executions (calculation results and timings) to a file
             out = new PrintStream("COMP5511/assignment1/LinearFib_out.txt");
+            // every System output sent to file
             System.setOut(out);
 
             int[] numbers = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
@@ -34,16 +38,21 @@ public class LinearFib {
                 long startTime = System.nanoTime();
                 BigInteger[] linearResult = linearFibonacci(number);
                 long endTime = System.nanoTime();
-                double duration = (endTime - startTime) / 1000000000.0;
-                out.println("linear Fibonacci of: " + number + " = " + linearResult[0] + " " + linearResult[1]);
-                out.println("execution time = " + duration + " seconds");
+
+                // execution time (nanoseconds) convert to seconds
+                double duration = (endTime - startTime) / 1_000_000_000.0;
+                
+                out.println("\n linear Fibonacci of: " + number + " = " + linearResult[0]);
+                out.println("\t sum of these two numbers: " + linearResult[1] + " + " + linearResult[2]);
+                out.println(String.format("\t execution time = %.9f",   duration) + " seconds");
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         long endMainTime = System.nanoTime();
-        double mainDuration = (endMainTime - startMainTime) / 1000000000.0;
-        out.println("TOTAL execution time = " + mainDuration);
+        // execution time (nanoseconds) convert to seconds
+        double mainDuration = (endMainTime - startMainTime) / 1_000_000_000.0;
+        out.println("\n\n TOTAL execution time = " + mainDuration + " seconds");
     }
 }
