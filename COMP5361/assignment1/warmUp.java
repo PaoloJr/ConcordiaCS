c80e85b8c9b5d3d6dc9a0755b3d4b60a085c32b1
Paolo <pj.angeloni@gmail.com>
2024-10-10 00:25:32 -0400

    almost done with single value entry

--- COMP5361/assignment1/warmUp.java
+++ COMP5361/assignment1/warmUp.java
@@ -1,63 +1,61 @@
 package COMP5361.assignment1;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 // code version 2
 public class warmUp {
-    private static boolean input1;
-    private static boolean input2;
+    // private static boolean input1;
+    // private static boolean input2;
 
-    private static Boolean negation(boolean value) {
+    private final Map<String, Boolean> truthMap;
+
+    public warmUp() {
+        truthMap = new HashMap<>();
+        truthMap.put("true", true);
+        truthMap.put("t", true);
+        truthMap.put("1", true);
+        truthMap.put("false", false);
+        truthMap.put("f", false);
+        truthMap.put("0", false);
+    }
+
+    private Boolean negation(boolean value) {
         return !value;
     }
 
     // user input values output to a String array
-    private static String[] getUserInput(Scanner scanner) {
+    private String[] getUserInput(Scanner scanner) {
         while (true) {
             System.out.print("Enter one or two (space-separated) truth values (True, False, T, F, 0, 1): ");
             String line = scanner.nextLine().trim();
             String[] inputs = line.split("\\s+");
             
             if (inputs.length == 1 || inputs.length == 2) {
-                try {
-                    if (inputs.length == 2) {
-                        return new String[]{inputs[0], inputs[1]};
-                    } else {
-                        return new String[]{inputs[0]};
-                    }
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                }
+                return inputs;
             } else {
                 System.out.println("Invalid input: " + line + "\nPlease enter one or two (space-separated) truth values.");
             }
         }
     }
 
-   // helper to validate user input and return the value entered to the user
-   // otherwise throw exception
-//   private static String parseBoolean(String input) {
-//        if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("t") || input.equals("1")) {
-//            return input;
-//        } else if (input.equalsIgnoreCase("false") || input.equalsIgnoreCase("f") || input.equals("0")) {
-//            return input;
-//        } else {
-//            throw new IllegalArgumentException("Invalid input: " + input + "\nMust be 'True', 'False', 'T', 'F', '0', or '1'.");
-//        }
-//    }
-
-    private static boolean parseBoolean(String input) {
-        boolean result = false;
-        if(input.equalsIgnoreCase("true") || input.equalsIgnoreCase("t") || input.equals("1")) {
-            result = true;
-        } else if (input.equalsIgnoreCase("false") || input.equalsIgnoreCase("f") || input.equals("0")) {
-            result = false;
+    private boolean parseBoolean(String input) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Boolean value = truthMap.get(input.toLowerCase());
+            if (value != null) {
+                return value;
+            } else {
+                System.out.println("Invalid input: " + input + "\nAll values must be either 'True', 'False', 'T', 'F', '0', or '1'.");
+                System.out.print("Please enter one or two (space-separated) truth values: ");
+                input = scanner.nextLine().trim();
+            }
         }
-        return result;
     }
 
     // format the returned value to be the same as the user input format
-    private static String getFormat(String original, boolean value) {
+    private String getFormat(String original, boolean value) {
         if (original.equalsIgnoreCase("true") || original.equalsIgnoreCase("false")) {
             return value ? "True" : "False";
         } else if (original.equalsIgnoreCase("t") || original.equalsIgnoreCase("f")) {
@@ -71,81 +69,22 @@ public class warmUp {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] values = getUserInput(scanner);
+        warmUp wu = new warmUp();
+
+        String[] values = wu.getUserInput(scanner);
         
+        // if only one value is entered execute negation only
         if (values.length == 1) {
-            System.out.println("You entered one valid value: " + values[0]);
-            boolean stringConvert = parseBoolean(values[0]);
-            String formatBoolean = getFormat(values[0], true);
-            boolean negatedValue = negation(input1);
+            System.out.println("You entered one valid truth value: " + values[0]);
+            boolean stringConvert = wu.parseBoolean(values[0]);
+            boolean negatedValue = wu.negation(stringConvert);
+            String formatBoolean = wu.getFormat(values[0], negatedValue);
             System.out.println("The negated value is: " + formatBoolean);
 
         } else if (values.length == 2) {
-            System.out.println("You entered two valid values: " + values[0] + " and " + values[1]);
+            System.out.println("You entered two valid truth values: " + values[0] + " and " + values[1]);
         }
         
         scanner.close();
     }
 }
-
-
-
-
-// code version 1
-
-// public static boolean negation(Object value) {
-//     if (value instanceof Boolean) {
-//         return !(Boolean) value;
-//     } else if (value instanceof String) {
-//         String strValue = (String) value;
-//         if ("true".equalsIgnoreCase(strValue) || "t".equalsIgnoreCase(strValue)) {
-//             return false;
-//         } else if ("false".equalsIgnoreCase(strValue) || "f".equalsIgnoreCase(strValue)) {
-//             return true;
-//         } else {
-//             throw new IllegalArgumentException("Invalid input. Please enter a boolean value. Either '0' and/or '1', 'True' and/or 'False', 'T' and/or 'F'");                
-//         }
-//     } else {
-//         throw new IllegalArgumentException("Invalid input. Please enter a boolean value. Either '0' and/or '1', 'True' and/or 'False', 'T' and/or 'F'");                
-//     }
-// }
-
-// public static void main(String[] args) {
-    //     Scanner scanner = new Scanner(System.in);
-    //     boolean value = getValidValue(scanner);
-    //     System.out.print("Enter a truth value: ");
-    //     boolean bool = scanner.nextBoolean();
-    //     String str = scanner.next("T""F");
-    //     scanner.close();
-    
-    //     boolean userInput = negation(bool);
-    //     System.out.println("Value entered: " + bool + "\nValue returned: " + userInput);
-        // boolean first = true;
-    // boolean second = false;
-    // int num = 1;
-    // boolean testFirst = negation(first);
-    // boolean testSecond = negation(second);
-    // System.out.println("Value entered: " + first + "\nValue returned: " + testFirst);
-    // System.out.println("Value entered: " + second + "\nValue returned: " + testSecond);
-    
-    // boolean error = negation(num);
-        // System.out.println(error);
-    // }
-    
-    // }
-
-    // public static boolean getValidValue(Scanner scanner) {
-    //     while (true) {
-    //         System.out.print("Enter a value (True, False, T, F, 0, 1): ");
-    //         String input = scanner.next().trim();
-    
-    //         // Check the valid cases ignoring case sensitivity
-    //         if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("t") || input.equals("1")) {
-    //             return true;
-    //         } else if (input.equalsIgnoreCase("false") || input.equalsIgnoreCase("f") || input.equals("0")) {
-    //             return false;
-    //         } else {
-    //             System.out.println("Invalid input. Please enter 'True', 'False', 'T', 'F', '0', or '1'.");
-    //         }
-    //     }
-    // }
