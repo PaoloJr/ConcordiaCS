53d234e93bbbe922b6d05d1128803fb8f263704b
Paolo <pj.angeloni@gmail.com>
2024-10-29 23:59:33 -0400

    tested new print

--- COMP5201/fun/helloWorld.asm
+++ COMP5201/fun/helloWorld.asm
@@ -3,6 +3,9 @@
 section .data
     msg db "Hello World!", 0xA          ; message to print with a newline character `0xA` = `\n` in ASCII
     msgLength equ $-msg                 ; dynamically get message buffer length
+    
+section .bss
+    inputTxt resd 2                     ; 2 double words --> 4 bytes 
 
 section .text
     global _start                       ; program must start with `_start`
@@ -12,6 +15,20 @@ _start:
     mov ebx, 1                          ; file descriptor = 
     mov ecx, msg                        ; store message in ecx
     mov edx, msgLength                  ; store length of message in edx
+    int 0x80
+
+    ; read input 
+    mov eax, 3                          ; sys_read
+    mov ebx, 2
+    mov ecx, inputTxt
+    mov edx, 4
+    int 0x80
+
+    ; print input
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, inputTxt
+    mov edx, 4
     int 0x80
 
     ; try to print the value of msgLength
