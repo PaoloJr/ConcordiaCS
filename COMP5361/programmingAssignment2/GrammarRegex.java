60265fca45095b084afe8d82dc0f34e508d337bd
Paolo <pj.angeloni@gmail.com>
2024-11-03 11:17:17 -0500

    working phone regex

--- COMP5361/programmingAssignment2/GrammarRegex.java
+++ COMP5361/programmingAssignment2/GrammarRegex.java
@@ -5,9 +5,32 @@ import java.io.FileNotFoundException;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GrammarRegex {
     
+
+    public static void main(String[] args) {
+    
+        String inputFile = "COMP5361/programmingAssignment2/IO/datePhoneIN.txt";
+        String outputFile = "COMP5361/programmingAssignment2/IO/datePhoneOUT.txt";
+
+        ArrayList<String> sentences = readSentencesFromFile(inputFile);
+
+        try (PrintStream out = new PrintStream(new File(outputFile))) {
+            for (String sentence : sentences) {
+            //    out.println(sentence);
+            String phone = phoneNumberRegex(sentence);
+            // String data = dateRegex(sentence);
+                out.println("Sentence: " + sentence);
+                out.println("Detected phone number: " + phone);
+                out.println();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error opening output file: " + outputFile);
+        }
+    }
 
     private static ArrayList<String> readSentencesFromFile(String fileName) {
         ArrayList<String> sentences = new ArrayList<>();
@@ -20,21 +43,25 @@ public class GrammarRegex {
             System.err.println("File not found: " + fileName);
         }
         return sentences;
-    }  
-
-    public static void main(String[] args) {
-    
-        String inputFile = "COMP5361/programmingAssignment2/IO/datePhoneIN.txt";
-        String outputFile = "COMP5361/programmingAssignment2/IO/datePhoneOUT.txt";
-
-        ArrayList<String> sentences = readSentencesFromFile(inputFile);
-
-        try (PrintStream out = new PrintStream(new File(outputFile))) {
-            for (String sentence : sentences) {
-               out.println(sentence);
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("Error opening output file: " + outputFile);
-        }
     }
+    
+    
+    private static String phoneNumberRegex(String sentence) {
+        // String phoneRegex = "\\+?1?[-.\\s]?\\(?[2-9]\\d{2}\\)?[-.\\s]?[2-9]\\d{2}[-.\\s]?\\d{4}";
+        String phoneRegex = "[(|+]?[1-9]?\\s?[(]?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}";
+        Pattern pattern = Pattern.compile(phoneRegex);
+        Matcher matcher = pattern.matcher(sentence);
+        
+        if (matcher.find()) {
+            return matcher.group().trim();
+            } else {
+                return "Phone Match Not Found!";
+            }
+        }
+                
+    // private static String dateRegex(String sentence) {
+        //     String dateRegex
+        
+        // }
+                    
 }
