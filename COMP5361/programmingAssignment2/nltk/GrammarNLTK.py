c62839721532b40b0a6b2260fb03560b4ba6d140
Paolo <pj.angeloni@gmail.com>
2024-11-05 00:06:06 -0500

    working read and write for python version

--- COMP5361/programmingAssignment2/nltk/GrammarNLTK.py
+++ COMP5361/programmingAssignment2/nltk/GrammarNLTK.py
@@ -7,7 +7,7 @@ grammar = CFG.fromstring("""
     Month -> 'January'|'February' 'March' 'April' 'May' 'June' 'July' 'August' 'September' 'October' 'November' 'December'
 """)
 list = list(generate(grammar))
-print(list)
+# print(list)
 
 
 
@@ -15,4 +15,19 @@ print(list)
 text = "Hello, world! This is a simple NLTK example. Written in November!"
 # Tokenize the text
 tokens = word_tokenize(text)
-print(tokens)
+# print(tokens)
+
+inputFile = "../IO/datePhoneIN_nltk.txt"
+outputFile = "../IO/datePhoneOUT_nltk.txt"
+
+readFile = open("../IO/datePhoneIN_nltk.txt")
+readFileLines = readFile.readlines()
+
+try:
+    with open(outputFile, 'x') as f:
+        f.write(readFileLines)
+except FileExistsError:
+    print(f"File '{outputFile}' already exists.")
+    print("Overwriting contents...")
+    with open(outputFile, 'w') as f:  # Open in 'w' mode to overwrite
+        f.writelines(readFileLines)
