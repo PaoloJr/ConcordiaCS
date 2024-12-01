9b586b819b601a3d9ace17b193d4c95e47743237
Paolo <pj.angeloni@gmail.com>
2024-11-30 21:05:53 -0500

    time regex almost done

--- COMP5361/pa3/GrammarNLTK.py
+++ COMP5361/pa3/GrammarNLTK.py
@@ -2,8 +2,6 @@ import os
 import nltk
 import re
 from nltk import CFG, ChartParser
-from nltk.tokenize import word_tokenize, line_tokenize, RegexpTokenizer, regexp_tokenize
-from nltk.parse.generate import generate
 
 #  Set the NLTK data path to the virtual environment's nltk_data directory
 nltk.data.path.append(os.path.join(os.path.dirname(__file__), 'nltk_data'))
@@ -13,10 +11,7 @@ script_dir = os.path.dirname(os.path.relpath(__file__))
 input_file = "./IO/datePhoneIN_nltk.txt"
 output_file = "./IO/datePhoneOUT_nltk.txt"
 input_path = os.path.join(script_dir, input_file)
-output_path = os.path.join(script_dir, output_file)
-
-phone_regex = re.compile(r"\(?[2-9][0-9]{2}\)?[-.\s]?[2-9][0-9]{2}[-.\s]?[0-9]{4}")  
-# phone_regex = RegexpTokenizer(r'\(?[2-9][0-9]{2}\)?[-.\s]?[2-9][0-9]{2}[-.\s]?[0-9]{4}')                        
+output_path = os.path.join(script_dir, output_file)                       
 
 phone_grammar = CFG.fromstring("""
 S -> phoneNumber
@@ -30,40 +25,6 @@ nonZeroOne -> '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 digit -> '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 delimiter -> '.' | '-' | ' ' |
 """)
-
-# date_regex = "\\b"
-# + "(?:" 
-# + "(?:((19|20)\\d{2})[-/.](0?[1-9]|1[0-2])[-/.](0?[1-9]|[12][0-9]|3[01]))"
-# + "|((0?[1-9]|[12][0-9]|3[01])[-/.](0?[1-9]|1[0-2])[-/.]((19|20)\\d{2}))"
-# + "|((0?[1-9]|1[0-2])[-/.](0?[1-9]|[12][0-9]|3[01])[-/.]((19|20)\\d{2}))"
-# + "|((19|20)\\d{2})[-/.]?(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/.](0?[1-9]|[12][0-9]|3[01])"
-# + "|((0?[1-9]|[12][0-9]|3[01])[-/.]?(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/.]((19|20)\\d{2}))"
-# + "|((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/.]?(0?[1-9]|[12][0-9]|3[01])[-/.]((19|20)\\d{2}))"
-# + "|(?:"
-# + "(?:Monday|Mon|Tuesday|Tue|Tues|Wednesday|Wed|Thursday|Thur|Thurs|Friday|Fri|Saturday|Sat|Sunday|Sun),?\\s*)?"
-# + "(?:January|Jan|February|Feb|March|Mar|April|Apr|May|June|Jun|July|Jul|August|Aug|September|Sept|Sep|October|Oct|November|Nov|December|Dec)\\s+"
-# + "(\\d{1,2}(?:st|nd|rd|th)?)"
-# + ",?\\s*(\\d{4})"
-# + ")\\b"
-
-date_regex = re.compile(r'''
-    (\b
-        (?: 
-            (?:
-                ((19|20)\d{2})[-/.](0?[1-9]|1[0-2])[-/.](0?[1-9]|[12][0-9]|3[01]))
-                |((0?[1-9]|[12][0-9]|3[01])[-/.](0?[1-9]|1[0-2])[-/.]((19|20)\d{2}))
-                |((0?[1-9]|1[0-2])[-/.](0?[1-9]|[12][0-9]|3[01])[-/.]((19|20)\d{2}))
-                |((19|20)\d{2})[-/.]?(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/.](0?[1-9]|[12][0-9]|3[01])
-                |((0?[1-9]|[12][0-9]|3[01])[-/.]?(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/.]((19|20)\d{2}))
-                |((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/.]?(0?[1-9]|[12][0-9]|3[01])[-/.]((19|20)\d{2}))
-            |(?:
-                (?:Monday|Mon|Tuesday|Tue|Tues|Wednesday|Wed|Thursday|Thur|Thurs|Friday|Fri|Saturday|Sat|Sunday|Sun),?\s*)?
-                (?:January|Jan|February|Feb|March|Mar|April|Apr|May|June|Jun|July|Jul|August|Aug|September|Sept|Sep|October|Oct|November|Nov|December|Dec)\s+
-                (\d{1,2}(?:st|nd|rd|th)?),?\s*(\d{4})
-        )
-    \b)
-    ''', re.VERBOSE|re.IGNORECASE
-)
 
 date_grammar = CFG.fromstring("""
     S ->  dateWithDelimiter | dateString 
@@ -89,23 +50,59 @@ date_grammar = CFG.fromstring("""
     space -> " "
 """)
 
-# phone_grammar.start()
-# phone_grammar.productions()
-# list(generate(phone_grammar))
-# print(list)
+time_grammar = CFG.fromstring("""
+    S -> 
+""")
 
-def detect_phone_number(sentence):
-    phone_number = re.findall(phone_regex, sentence)
-    # phone_number = regexp_tokenize(sentence, phone_regex)
-    # return phone_regex.tokenize(sentence)
-    return phone_number
+phone_regex = re.compile(r"\(?[2-9][0-9]{2}\)?[-.\s]?[2-9][0-9]{2}[-.\s]?[0-9]{4}")  
+
+date_regex = re.compile(r'''
+    \b
+    (
+        (?:
+            (?: (?:19|20)\d{2} ) [-/.] (?:0?[1-9]|1[0-2]) [-/.] (?:0?[1-9]|[12][0-9]|3[01])
+            |(?:0?[1-9]|[12][0-9]|3[01]) [-/.] (?:0?[1-9]|1[0-2]) [-/.] (?:19|20)\d{2}
+            |(?:0?[1-9]|1[0-2]) [-/.] (?:0?[1-9]|[12][0-9]|3[01]) [-/.] (?:19|20)\d{2}
+            |(?:19|20)\d{2} [-/.]? (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [-/.] (?:0?[1-9]|[12][0-9]|3[01])
+            |(?:0?[1-9]|[12][0-9]|3[01]) [-/.]? (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [-/.] (?:19|20)\d{2}
+            |(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [-/.]? (?:0?[1-9]|[12][0-9]|3[01]) [-/.] (?:19|20)\d{2}
+            |(?:
+                (?:Monday|Mon|Tuesday|Tue|Tues|Wednesday|Wed|Thursday|Thur|Thurs|Friday|Fri|Saturday|Sat|Sunday|Sun),?\s*)?
+            (?:January|Jan|February|Feb|March|Mar|April|Apr|May|June|Jun|July|Jul|August|Aug|September|Sept|Sep|October|Oct|November|Nov|December|Dec)\s+
+            \d{1,2}(?:st|nd|rd|th)?,?\s*\d{4}
+        )
+    )
+    \b
+    ''', re.VERBOSE | re.IGNORECASE)
+
+time_regex = re.compile(r"")
+
+# phone_tokenizer = nltk.RegexpTokenizer(phone_regex)
+# date_tokenizer = nltk.RegexpTokenizer(date_regex) 
+# time_tokenizer = nltk.RegexpTokenizer(time_regex)
+
+phone_number_parser = nltk.ChartParser(phone_grammar)
+date_parser = nltk.ChartParser(date_grammar)
+# time_parser = nltk.ChartParser(time_grammer)
+
+parse_phone = nltk.RecursiveDescentParser(phone_grammar)
+parse_date = nltk.RecursiveDescentParser(date_grammar)
+# parse_time = nltk.RecursiveDescentParser(time_grammar)
 
 def detect_date(sentence):
     date = re.findall(date_regex, sentence)
     # print(date)
-    return [''.join(filter(None, match)) for match in date]
-    # print(date_regex.tokenize(sentence))
-    # return date_regex.tokenize(sentence)
+    return date
+
+def detect_phone_number(sentence):
+    phone_number = re.findall(phone_regex, sentence)
+    # print(phone_number)
+    return phone_number
+
+def detect_time(sentence):
+    time = re.findall(time_regex, sentence)
+    # print(time)
+    return time
 
 def process_files(input_path, output_path):
     if not os.path.isfile(input_path):
@@ -113,27 +110,47 @@ def process_files(input_path, output_path):
     else:
         with open(input_path, 'r') as read_file, open(output_path, 'w') as write_file:
             read_file_lines = read_file.readlines()
-            for line in read_file_lines:
+            for sentence in read_file_lines:
                 try:
-                    for sentence in read_file_lines:
-                        # detect phone number and date
-                        phone_numbers = detect_phone_number(sentence)
-                        dates = detect_date(sentence)
-                        
-                        # print(f"Sentence: \"{sentence}\"")
-                        write_file.write(f"Sentence: :\" {sentence}")
-                        for phone_number in phone_numbers:
-                            # print(f"Detected Phone Number: {phone_number}")
-                            write_file.write(f"Detected Phone Number: {phone_number}\n")
-                        for date in dates:
-                            # print(f"Detected Date: {date}")
-                            write_file.write(f"Detected Date: {date}\n")
-                        # print()
+                    # detect phone number and date
+                    phone_number_found = detect_phone_number(sentence)
+                    date_found = detect_date(sentence)
+                    time_found = detect_time(sentence)
+                    # phone_tokens = phone_tokenizer.tokenize(sentence)
+                    # date_tokens = date_tokenizer.tokenize(sentence)
+
+                    if "*****" in sentence:
                         write_file.write("\n")
+                        write_file.write("\n")
+                        write_file.write("-----*****NON-ACCEPTABLE-FORMATS-BELOW*****-----\n")
+                        continue
+
+                    write_file.write(f"Sentence: {sentence.strip()}\n")
+                    if phone_number_found:
+                    # if phone_tokens:
+                        # for phone_number in phone_numbers:
+                        for phone_number in phone_number_found:
+                        # for phone_number in phone_tokens:
+                            write_file.write(f"Detected Phone Number: {phone_number}\n")
+                    else:
+                            write_file.write("NO PHONE DETECTED\n")
+                    if date_found:
+                    # if date_tokens:
+                        for date in date_found:
+                        # for date in date_tokens:
+                            write_file.write(f"Detected Date: {date}\n")
+                    else:
+                            write_file.write("NO DATE DETECTED\n")
+                    if time_found:
+                        for time in time_found:
+                             write_file.write(f"Detected Time: {time}\n")
+                    else:
+                            write_file.write(f"NO TIME DETECTED\n")
+                    write_file.write("\n")
                 except ValueError as e:
-                        # print(f"Error parsing ling: {line.strip()}")
-                        # print(e)
-                        write_file(f"Error parsing line: {line.strip()}\n")
-                        write_file.write(f"{e}\n")
+                        print(f"Error parsing ling: {sentence.strip()}")
+                        print(e)
+                        # write_file(f"Error parsing line: {sentence.strip()}\n")
+                        # write_file.write(f"{e}\n")
 
 process_files(input_path, output_path)
