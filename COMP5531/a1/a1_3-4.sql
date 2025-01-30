2a66f3165773cea42dab1c39c908e4725ec7f006
Paolo <pj.angeloni@gmail.com>
2025-01-30 17:32:36 -0500

    int for model

--- COMP5531/a1/a1_3-4.sql
+++ COMP5531/a1/a1_3-4.sql
@@ -11,24 +11,24 @@ USE assignment1;
 
 CREATE TABLE Product(
 	maker VARCHAR(30),
-	model VARCHAR(30),
+	model INTEGER,
     type VARCHAR(30)
 );
 
-INSERT INTO Product VALUES ('dell', 'xps', 'laptop'), ('system76', 'mega', 'pc'), ('acer', 'aspire', 'laptop'), ('hp', 'elitebook', 'laptop'), ('lenovo', 'yoga', 'laptop'), ('lenovo', 'thinkpad', 'laptop'), ('asrock', 'tb3', 'pc');
+INSERT INTO Product VALUES ('dell', 1, 'laptop'), ('system76', 2, 'pc'), ('acer', 3, 'laptop'), ('hp', 4, 'laptop'), ('lenovo', 5, 'laptop'), ('framework', 6, 'laptop'), ('asrock', 7, 'pc');
 
 CREATE TABLE PC (
-	model VARCHAR(30),
+	model INTEGER,
     speed FLOAT(2,1),
     ram INTEGER,
     hd INTEGER,
     price INTEGER
 );
 
-INSERT INTO PC VALUES ('mega', 2.1, 4, 256, 2500), ('mega', 2.6, 8, 256, 5500), ('asrock', 3.9, 8, 512, 6500), ('asrock', 2.5, 2, 128, 1500);
+INSERT INTO PC VALUES (2, 2.1, 4, 256, 2500), (2, 2.6, 8, 256, 5500), (7, 3.9, 8, 512, 6500), (7, 2.5, 2, 128, 1500);
 
 CREATE TABLE Laptop (
-	model VARCHAR(30),
+	model INTEGER,
     speed INTEGER,
     ram INTEGER,
     hd INTEGER,
@@ -36,7 +36,7 @@ CREATE TABLE Laptop (
     price INTEGER
 );
 
-INSERT INTO Laptop VALUES ('xps', 2.0, 8, 512, 'HD', 2000), ('aspire', 1.8, 12, 128, 'SD', 750), ('elitebook', 2.4, 16, 256, 'HD', 1000), ('yoga', 2.2, 12, 512, 'HD', 1200), ('thinkpad', 3.2, 32, 512, 2000);
+INSERT INTO Laptop VALUES (1, 2.0, 8, 512, 'HD', 2000), (3, 1.8, 12, 128, 'SD', 750), (4, 2.4, 16, 256, 'HD', 1000), (5, 2.2, 12, 512, 'HD', 1200), (6, 3.2, 32, 512, 2000);
 
 
 
