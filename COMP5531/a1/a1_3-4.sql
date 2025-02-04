dc9bc3f47274173105a5daaf76545f268a85bd0e
Paolo <pj.angeloni@gmail.com>
2025-02-04 00:35:34 -0500

    almost done q3 and q4

--- COMP5531/a1/a1_3-4.sql
+++ COMP5531/a1/a1_3-4.sql
@@ -9,34 +9,59 @@ CREATE DATABASE IF NOT EXISTS assignment1;
 
 USE assignment1;
 
-CREATE TABLE Product(
-	maker VARCHAR(30),
-	model INTEGER,
-    type VARCHAR(30)
+CREATE TABLE IF NOT EXISTS Product(
+	maker VARCHAR(30) NOT NULL,
+	model INTEGER UNSIGNED PRIMARY KEY,
+    type ENUM('pc', 'laptop', 'printer') NOT NULL
 );
 
-INSERT INTO Product VALUES ('dell', 1, 'laptop'), ('system76', 2, 'pc'), ('acer', 3, 'laptop'), ('hp', 4, 'laptop'), ('lenovo', 5, 'laptop'), ('framework', 6, 'laptop'), ('asrock', 7, 'pc');
+INSERT INTO Product VALUES ('dell', 1, 'laptop'), ('system76', 2, 'pc'), ('acer', 3, 'laptop'), ('hp', 4, 'laptop'), ('lenovo', 5, 'laptop'), ('framework', 6, 'laptop'), ('asrock', 7, 'pc'), ('hp', 20, 'printer'), ('brother', 21, 'printer'), ('xerox', 22, 'printer'), ('epson', 23, 'printer'), ('test', null, 'printer');
 
-CREATE TABLE PC (
-	model INTEGER,
-    speed FLOAT(2,1),
-    ram INTEGER,
-    hd INTEGER,
-    price INTEGER
+CREATE TABLE IF NOT EXISTS PC (
+	model INTEGER UNSIGNED PRIMARY KEY,
+    speed DECIMAL(3,2) NOT NULL,
+    ram INTEGER UNSIGNED NOT NULL,
+    hd INTEGER UNSIGNED NOT NULL,
+    price INTEGER UNSIGNED NOT NULL
 );
 
-INSERT INTO PC VALUES (2, 2.1, 4, 256, 2500), (2, 2.6, 8, 256, 5500), (7, 3.9, 8, 512, 6500), (7, 2.5, 2, 128, 1500);
+INSERT INTO PC VALUES (2, 2.15, 4, 256, 2500), (2, 2.15, 8, 256, 5500), (7, 3.95, 8, 512, 6500), (7, 3.95, 2, 128, 1500);
 
-CREATE TABLE Laptop (
-	model INTEGER,
-    speed INTEGER,
-    ram INTEGER,
-    hd INTEGER,
-    screen VARCHAR(20),
-    price INTEGER
+CREATE TABLE IF NOT EXISTS Laptop (
+	model INTEGER UNSIGNED PRIMARY KEY,
+    speed DECIMAL(3,2) NOT NULL,
+    ram INTEGER UNSIGNED NOT NULL,
+    hd INTEGER UNSIGNED NOT NULL,
+    screen DECIMAL(3,1) NOT NULL,
+    price INTEGER UNSIGNED NOT NULL
 );
 
-INSERT INTO Laptop VALUES (1, 2.0, 8, 512, 'HD', 2000), (3, 1.8, 12, 128, 'SD', 750), (4, 2.4, 16, 256, 'HD', 1000), (5, 2.2, 12, 512, 'HD', 1200), (6, 3.2, 32, 512, 2000);
+INSERT INTO Laptop VALUES (1, 2.05, 8, 512, 15.4, 2000), (3, 1.85, 12, 128, 13.3, 750), (4, 2.45, 16, 256, 16.1, 1000), (5, 2.25, 12, 512, 12.2, 1200), (6, 3.25, 32, 512, 17.1, 2000);
+
+CREATE TABLE IF NOT EXISTS Printer (
+    model INTEGER UNSIGNED PRIMARY KEY,
+    color BOOLEAN NOT NULL,
+    type ENUM('laser', 'ink-jet') NOT NULL,
+    price INTEGER UNSIGNED NOT NULL
+);
+
+INSERT INTO Printer VALUES (10, 1, 'laser', 500), (11, 0, 'ink-jet', 250), (12, 0, 'ink-jet', 650), (13, 1, 'laser', 700), (14, 2, 'laser', 150);
 
 
+-- alterations
+ALTER TABLE Laptop 
+ADD od ENUM('cd', 'dvd', 'none') DEFAULT 'none' NOT NULL;
+
+-- to edit existing column
+-- ALTER TABLE table_name
+-- MODIFY COLUMN column_name new_data_type;
+
+-- first defaults `od` to 'none'
+INSERT INTO Laptop VALUES (6, 3.2, 16, 256, 15.4, 1500, 'none'), (1, 2.0, 12, 128, 15.4, 1500, 'cd'), (4, 2.4, 24, 512, 16.1, 1250, 'dvd');
+
+ALTER TABLE Printer
+DROP color;
+
+
+-- SELECT queries
 
